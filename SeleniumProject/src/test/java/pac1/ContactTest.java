4e364153b589794caa519744feca4a1c27513514
obisw <obisw@bj>
2026-09-08 12:23:44 +0530

    added new test

--- SeleniumProject/src/test/java/pac1/ContactTest.java
+++ SeleniumProject/src/test/java/pac1/ContactTest.java
@@ -23,4 +23,9 @@ public class ContactTest {
 	public void modifycontactTest() {
 		System.out.println("execute modifycontactTest ");
 	}
+	
+	@Test
+	public void deletecontactTest() {
+		System.out.println("execute modifycontactTest ");
+	}
 }
